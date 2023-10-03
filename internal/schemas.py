4d9f7b7361be7884eae6d8f88efc6b31a3f5a870
albertmus123 <mualbert2@gmail.com>
2023-10-03 17:41:13 +0200

    hhh

--- internal/schemas.py
+++ internal/schemas.py
@@ -56,6 +56,14 @@ class Order(BaseModel):
     Products : ShowProductCart
     users : ShowUser
     
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+    
 
     
     
